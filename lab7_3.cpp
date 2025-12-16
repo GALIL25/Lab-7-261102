9bcc651c29e8011103b21b85a161cdbbc52d6fb7
GARLIL25 <36127@dara.ac.th>
2025-12-16 22:46:47 +0700

    lab7_3.cpp last oneeee

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x) {
+    if (x < 'A' || x > 'Z') {
+        return '0';
+    }
+    if (x == 'A') {
+        return 'Z';
+    }
+    return x - 1;
 }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+int main() {
+    cout << before('A') << "\n"; // Z
+    cout << before('B') << "\n"; // A
+    cout << before('P') << "\n"; // O
+    cout << before('T') << "\n"; // S
+    cout << before('Z') << "\n"; // Y
+    cout << before('a') << "\n"; // 0
+    cout << before('0') << "\n"; // 0
+    cout << before('c') << "\n"; // 0
+
+    return 0;
 }
